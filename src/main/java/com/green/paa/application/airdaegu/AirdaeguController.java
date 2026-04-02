8ba10f4b7f36250782abcbaeb28d2699519f6a9b
sbsteacher <id@gmail.com>
2026-04-02 16:25:31 +0900

    Airdaegu-3

--- src/main/java/com/green/paa/application/airdaegu/AirdaeguController.java
+++ src/main/java/com/green/paa/application/airdaegu/AirdaeguController.java
@@ -1,4 +1,24 @@
 package com.green.paa.application.airdaegu;
 
+import com.green.paa.application.airdaegu.model.AirdaeguItem;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/airdaegu")
 public class AirdaeguController {
+    private final AirdaeguService airdaeguService;
+
+    @GetMapping
+    public List<AirdaeguItem> getAirdaeguList(@RequestParam String dataDt) {
+        return airdaeguService.getAirdaeguList(dataDt);
+    }
 }
