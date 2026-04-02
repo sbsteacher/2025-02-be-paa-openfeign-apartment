112a12fd5a5c322387fa2d5fbfe13776847c6520
sbsteacher <id@gmail.com>
2026-04-02 16:10:56 +0900

    Airdaegu-2

--- src/main/java/com/green/paa/application/airdaegu/AirdaeguClient.java
+++ src/main/java/com/green/paa/application/airdaegu/AirdaeguClient.java
@@ -1,14 +1,12 @@
 package com.green.paa.application.airdaegu;
 
-import com.green.paa.application.airdaegu.model.AirdaeguItem;
+import com.green.paa.application.airdaegu.model.AirdaeguResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @FeignClient(name="airdaegu-api", url="https://air.daegu.go.kr/api/xml/dayavg.do")
 public interface AirdaeguClient {
     @GetMapping(produces = "application/xml")
-    List<AirdaeguItem> getAirdaegu(@RequestParam("data_dt") String dataDt);
+    AirdaeguResponse getAirdaegu(@RequestParam("data_dt") String dataDt);
 }
--- src/main/java/com/green/paa/application/airdaegu/AirdaeguService.java
+++ src/main/java/com/green/paa/application/airdaegu/AirdaeguService.java
@@ -1,4 +1,22 @@
 package com.green.paa.application.airdaegu;
 
+import com.green.paa.application.airdaegu.model.AirdaeguItem;
+import com.green.paa.application.airdaegu.model.AirdaeguResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class AirdaeguService {
+    private final AirdaeguClient airdaeguClient;
+
+    public List<AirdaeguItem> getAirdaeguList(String dataDt) {
+        AirdaeguResponse response = airdaeguClient.getAirdaegu(dataDt);
+        return response.getRows();
+    }
 }
